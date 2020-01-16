37670fa99fa43b435bd808390f8e61f859926d98
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-01-16 11:17:20 +0000

    Details view created

--- src/views/Details.jsx
+++ src/views/Details.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import { Container, Text } from '../components/index';
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { Container, Text, Image } from '../components/index';
 import {
   BtnBorrow,
-  Image,
   TextContainer,
   Holder,
 } from '../components/details/controllers';
 
-export default () => (
-  <Container flex justify="center">
-    <Holder>
-      <Image />
-      <TextContainer>
-        <Text size="35px" color="black">Name: Nazarov Ahmad</Text>
-        <Text size="35px" color="black">Student ID: U1710073</Text>
-        <Text size="35px" color="black">Department: SOL</Text>
-        <Text size="35px" color="black">Status: No overdue</Text>
-        <Text size="35px" color="black">Type:   Student</Text>
-      </TextContainer>
-    </Holder>
-    <BtnBorrow>
-      <Text size="30px" color="white">START BORROWING</Text>
-    </BtnBorrow>
-  </Container>
-);
+const Details = ({ student }) => {
+  const history = useHistory();
+  return student ? (
+    <Container flex justify="center">
+      <Holder>
+        <Image src={`${process.env.REACT_APP_SERVER_URL}${student.image}`} height="350px" width="350px" />
+        <TextContainer>
+          <Text size="35px" color="grey">{`Name: ${student.secondName} ${student.name}`}</Text>
+          <Text size="35px" color="grey">{`Student ID: ${student.studentId}`}</Text>
+          <Text size="35px" color="grey">{`Department: ${student.department}`}</Text>
+          <Text size="35px" color="grey">{`Status: ${student.status}`}</Text>
+          <Text size="35px" color="grey">{`Type: ${student.type}`}</Text>
+        </TextContainer>
+      </Holder>
+      <BtnBorrow onClick={() => history.push('/home')}>
+        <Text size="30px" color="white">START BORROWING</Text>
+      </BtnBorrow>
+    </Container>
+  ) : <></>;
+};
+
+const stateToProps = (state) => ({ student: state.students.student });
+
+export default connect(stateToProps)(Details);
